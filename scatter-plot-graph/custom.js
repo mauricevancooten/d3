8ad4d44c4fc61cb39fba99fd3095755e1e07aff6
Maurice van Cooten <maurice@minus.nz>
2016-05-29 15:08:49 +1200

    scatter plot graph example

---
+++ scatter-plot-graph/custom.js
@@ -0,0 +1,31 @@
+(function() {
+
+	var dataset = [
+		[140, 80],
+		[75, 69],
+		[111, 55], 
+		[20, 54],
+		[49, 63],
+		[190, 180]
+	];
+
+	var w = 200;
+	var h = 200;
+
+	var svg = d3.select('.container').append('svg')
+	.attr('width', w)
+	.attr('height', h)
+
+	svg.selectAll('circle')
+	.data(dataset)
+	.enter()
+	.append('circle')
+	.attr('cx', function(d){
+		return d[0];
+	})
+	.attr('cy', function(d){
+		return d[1];
+	})
+	.attr('r', 5);
+
+})();
