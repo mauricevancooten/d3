3c11f93e7618c075a717d7e382f6afe52a0e6e40
Maurice van Cooten <maurice@minus.nz>
2016-05-29 15:08:21 +1200

    svg example

---
+++ svg/custom.js
@@ -0,0 +1,26 @@
+(function() {
+
+	var svg = d3.select('.container').append('svg');
+
+	var	w = 500;
+	var h = 60;
+	
+	svg.attr('width', w)
+	.attr('height', h);
+ 	
+ 	var dataset = [5, 10, 15, 20, 25, 20, 15, 10, 5];
+
+ 	var circles = svg.selectAll('circle')
+ 	.data(dataset)
+ 	.enter()
+ 	.append('circle');
+
+ 	circles.attr('cx', function(d, i) {
+ 		return (i * 50) + 50;
+ 	})
+ 	.attr('cy', h/2)
+ 	.attr('r', function(d) {
+ 		return d;
+ 	});
+
+})();
