8159b5090a24f2a894133c37cadbf5810447a585
Maurice van Cooten <maurice@minus.nz>
2016-05-29 15:08:06 +1200

    bar-graph example

---
+++ bar-graph/custom.js
@@ -0,0 +1,14 @@
+(function() {
+
+	var dataset = [140, 80, 75, 69, 111, 55];
+
+	d3.select('.container').selectAll('div')
+	.data(dataset)
+	.enter()
+	.append('div')
+	.attr('class', 'bar')
+	.style('height', function(d) {
+	 		return d + 'px';
+	});
+
+})();
